dd3ace9fd2d9c9adc114ef51be63161af817e1dc
Mari Nahapetyan <marie.nahapetian@gmail.com>
2025-06-12 16:37:18 +0200

    add api endpoints

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
-    //
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function attendees()
+    {
+        return $this->hasMany(Attendee::class);
+    }
 }
