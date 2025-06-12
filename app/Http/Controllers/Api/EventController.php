548b7d71cb663055ac4e2f1227894e8551b7bed3
Mari Nahapetyan <marie.nahapetian@gmail.com>
2025-06-12 23:18:25 +0200

    add model factories,seeders, create resources for models

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\EventResource;
+use App\Models\Event;
 use Illuminate\Http\Request;
 
 class EventController extends Controller
@@ -12,7 +14,8 @@ class EventController extends Controller
      */
     public function index()
     {
-        //
+        //return Event::all();
+        return EventResource::collection(Event::with('user', 'attendees')->get());
     }
 
     /**
@@ -20,30 +23,51 @@ class EventController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $event = Event::create([
+            ...$request->validate([
+                'name' => 'required | string | max:255',
+                'description' => 'nullable | string',
+                'start_time' => 'required | date',
+                'end_time' => 'required | date | after:start_time'
+            ]),
+            'user_id' => 1
+        ]);
+
+        return $event;
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Event $event)
     {
-        //
+        return new EventResource($event->load('user', 'attendees'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Event $event)
     {
-        //
+        $event->update($request->validate([
+            'name' => 'sometimes | string | max:255',
+            'description' => 'nullable | string',
+            'start_time' => 'sometimes | date',
+            'end_time' => 'sometimes | date | after:start_time'
+        ]));
+
+        return new EventResource($event);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Event $event)
     {
-        //
+        $event->delete();
+
+        return response()->json([
+            'message' => 'event deleted successfully'
+        ]);
     }
 }
